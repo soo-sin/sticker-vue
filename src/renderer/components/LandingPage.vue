3ebd15594831e1ec5a64e7bc5702698c96420b56
soo-sin <pcs7993@gmail.com>
2020-12-17 02:18:07 +0900

    Initial commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <textarea id="note">
+  </textarea>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    components: { },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    /*box-sizing: border-box;*/
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    height: 100vh;
+    width: 100vw;
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  #note {
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    resize: none;
+    font-size: 30px;
+  }
+
+</style>
